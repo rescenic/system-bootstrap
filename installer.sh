32df1b8719582235db686e24e75473aa80f2e32b
Vlad Doster <mvdoster@gmail.com>
2019-11-24 18:59:45 -0500

    Update installer.sh
    updated old programs reference

--- installer.sh
+++ installer.sh
@@ -15,7 +15,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 esac done
 
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$progsfile" ] && progsfile="https://github.com/vladdoster/dotfile-installer/blob/master/programs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
