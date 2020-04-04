be323a5a98cf35a3bbf2c3e3616eb1901c788d09
Vlad Doster <mvdoster@gmail.com>
2020-04-04 04:17:36 -0500

    Update dotfile-installer.sh

--- dotfile-installer.sh
+++ dotfile-installer.sh
@@ -256,8 +256,8 @@ newperms "%wheel ALL=(ALL) ALL #dotfile-installer
 %wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman -Syyuw --noconfirm,/usr/bin/nmtui"
 
 # Check which programs arent present programs.csv
-potential_packages=$(curl -s $progsfile | sed '/^#/d')
-num_possible_packages=$(potential_packages | wc -l)
+potential_packages="$(curl -s $progsfile | sed '/^#/d')"
+num_possible_packages=$($potential_packages | wc -l)
 unsuccessfully_installed_programs=$(printf "\n" && echo "$(curl -s $progsfile | sed '/^#/d')" | while IFS=, read -r tag program comment; do if [[ $tag == 'G' ]]; then printf "%s\n" "$program"; elif [[ "$(pacman -Qi "$program" > /dev/null)" ]]; then printf "%s\n" "$program"; fi; done)
 
 # Install complete!
