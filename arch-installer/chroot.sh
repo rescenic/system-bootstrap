8867cdc4f6ddbcc697ddcbdde18a7fc5d7de4bee
Vlad Doster <mvdoster@gmail.com>
2020-05-21 01:49:57 -0500

    Update chroot.sh

--- arch-installer/chroot.sh
+++ arch-installer/chroot.sh
@@ -1,4 +1,6 @@
-# Potential variables: timezone, lang and local
+# Potential variables: timezone, lang, local, drive, and bootloader partition
+pacman --noconfirm --needed -Sy dialog intel-ucode reflector networkmanager >/dev/null 2>&1
+
 if [ $# -ne 2 ]; then
     dialog --title "BOOMER BRAINLET" --infobox "Something went wrong. Chroot did not receive 2 arguments.\It needs the drive and the bootloader partition." 7 50
     exit 1
@@ -11,7 +13,7 @@ dialog --title "Dotfile installer" --infobox "Arch was installed on: ${drive}" 7
 sleep 10
 
 dialog --title "Dotfile installer" --infobox "Installing bootloader on ${bootloader_partition}" 7 50
-pacman --noconfirm --needed -S intel-ucode
+sleep 5
 UUID=$(blkid -s PARTUUID -o value ${bootloader_partition})
 bootctl install || error "Installing bootctl"
 echo title Arch Linux >> /boot/loader/entries/arch.conf
@@ -20,30 +22,25 @@ echo initrd /intel-ucode.img >> /boot/loader/entries/arch.conf
 echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf
 echo options root=PARTUUID=${UUID} >> /boot/loader/entries/arch.conf
 
-pacman --noconfirm --needed -S dialog reflector || { echo "Error at script start: Are you sure you're running this as the root user? Are you sure you have an internet connection?"; exit; }
-dialog --title "Dotfile installer" --infobox "Installing and running \`reflector\` for fastest possible download speeds." 5 70
-reflector --verbose --latest 100 --sort rate --save /etc/pacman.d/mirrorlist &> /dev/null
-clear
+dialog --title "Dotfile installer" --infobox "Updating pacman mirrors." 5 70
+reflector --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist &> /dev/null
 
-error() { clear; printf "ERROR:\\n%s\\n" "$1"; exit;}
-
-# System password
+# Set system password
 passwd
 
-# System timezone
+# Set system timezone
 TZuser=$(cat tzfinal.tmp)
 ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
 hwclock --systohc
 
-# System locale
+# Set system locale
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
 
-pacman --noconfirm --needed -S networkmanager intel-ucode
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
 installdotfiles() { curl -O https://raw.githubusercontent.com/vladdoster/dotfile-installer/master/dotfile-installer.sh && bash dotfile-installer.sh ;}
-dialog --title "Install dotfiles?" --yesno "This install script will easily let you access boostrapping scripts which automatically install a full Arch Linux i3-gaps desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no."  15 60 && installdotfiles
+dialog --title "Install dotfiles?" --yesno "Install dotfiles.vdoster.com?" 3 60 && installdotfiles
