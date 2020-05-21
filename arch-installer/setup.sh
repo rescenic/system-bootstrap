e8889bdd2ca19e53dfa9f4b156fad26342160563
Vlad Doster <mvdoster@gmail.com>
2020-05-21 02:11:51 -0500

    Update setup.sh

--- arch-installer/setup.sh
+++ arch-installer/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo pacman -Syy && yes | sudo pacman -S git && git clone https://github.com/vladdoster/dotfile-installer
-chmod 777 -R arch-installer/
-cd dotfile-installer/arch-installer/
+sudo pacman -Syy && yes | sudo pacman -S git && git clone https://github.com/vladdoster/system-installer .
+chmod --recursive 777 system-installer/
+
