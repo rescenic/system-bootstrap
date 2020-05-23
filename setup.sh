def8cfe54f1ff0a10561eccc7c13c5daeb4eb153
Vlad Doster <mvdoster@gmail.com>
2020-05-23 00:15:29 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yes | sudo pacman -Syy git
 git clone https://github.com/vladdoster/system-installer 2> /dev/null || echo "Already cloned"
-rsync -r --verbose --exclude-from=.copyignore system-installer .
+rsync --recursive --verbose --exclude-from=.copyignore ./system-installer/ $(pwd)
 # (cd system-installer/; shopt -s extglob; cp -r !(LICENSE | README | git) ../)
-rm -r system-installer/
+rm --recursive --verbose ./system-installer
