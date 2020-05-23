368d9ea51ade51cfe284e84e9f019d75c7707f40
Vlad Doster <mvdoster@gmail.com>
2020-05-23 09:32:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yes | sudo pacman --quiet -S git
-git clone https://github.com/vladdoster/system-installer 2> /dev/null || echo "Already cloned"
-cp --recursive ./system-installer/* $(pwd); rm --recursive ./system-installer/ LICENSE README.md; chmod +x *.sh
+git clone https://github.com/vladdoster/personal-system-installer 2> /dev/null || echo "Already cloned"
+cp --recursive ./personal-system-installer/* $(pwd); rm --recursive ./personal-system-installer/ LICENSE README.md; chmod +x *.sh
 echo "Install Arch: sudo ./arch-install.sh"
