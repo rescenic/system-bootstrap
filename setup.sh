ed89e22993933c9924abc43158fd59bbcbd7db8a
Vlad Doster <mvdoster@gmail.com>
2020-05-22 23:24:00 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 LOCALREPO_VC_DIR="system-installer"
 yes | sudo pacman -Syy git
 git clone https://github.com/vladdoster/system-installer 2> /dev/null || echo "Already cloned"
-cp -r system-installer/. .
-rm -r system-installer/
+# cp -r system-installer/. .
+# rm -r system-installer/
+rsync -av system-installer/ . --exclude LICENSE --exclude README.md
