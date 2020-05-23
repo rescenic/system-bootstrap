150c3986f61c1634bc5e050cb9caffbc2be3f945
Vlad Doster <mvdoster@gmail.com>
2020-05-23 02:50:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-yes | sudo pacman -S git
+yes | sudo pacman --quiet -S git
 git clone https://github.com/vladdoster/system-installer 2> /dev/null || echo "Already cloned"
-cp --recursive ./system-installer/* $(pwd)
-rm --recursive .git/ LICENSE README.md
-chmod +x *.sh
+cp --recursive ./system-installer/* $(pwd); rm --recursive LICENSE README.md; chmod +x *.sh
 echo "To start install, sudo ./install-arch"
