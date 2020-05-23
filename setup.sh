c3a8a8571980bbd5fd35253a81de2bccbce0a3d4
Vlad Doster <mvdoster@gmail.com>
2020-05-22 23:53:29 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 yes | sudo pacman -Syy git
 git clone https://github.com/vladdoster/system-installer 2> /dev/null || echo "Already cloned"
 (cd system-installer/; shopt -s extglob; cp -r !(LICENSE | README | git) ../)
