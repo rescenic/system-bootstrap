f7460039fbb698e8669e0604d8c3608077b5bbdc
Vlad Doster <mvdoster@gmail.com>
2020-05-23 00:28:21 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yes | sudo pacman -Syy git
 git clone https://github.com/vladdoster/system-installer 2> /dev/null || echo "Already cloned"
-rsync --recursive --verbose --exclude ".git/" --exclude "LICENSE" --exclude "README.md" ./system-installer/ "$(pwd)"
-rm --recursive ./system-installer
+rsync --exclude={'.git/','LICENSE','README.md'} system-installer/ $(pwd)
+rm --recursive system-installer/
