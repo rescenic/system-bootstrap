e708c2ca646162a62c98069a44a161b134cd4028
Vlad Doster <mvdoster@gmail.com>
2020-05-25 04:56:17 -0500

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -7,22 +7,6 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-drive="$1"
-bootloader_partition="$2"
-
-dialog --title "Dotfile installer" --infobox "Arch was installed on ${drive}!" 3 50
-
-# dialog --title "Dotfile installer" --infobox "Updating pacman mirrors." 3 70
-# reflector --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
-UUID=$(blkid -s PARTUUID -o value ${bootloader_partition})
-dialog --title "Dotfile installer" --infobox "Installing bootloader on ${bootloader_partition} with UUID ${UUID}" 3 50
-bootctl install >/dev/null 2>&1 || echo "Bootctl seemed to hit a snag..."
-echo title Arch Linux >> /boot/loader/entries/arch.conf
-echo linux /vmlinuz-linux >> /boot/loader/entries/arch.conf
-echo initrd /intel-ucode.img >> /boot/loader/entries/arch.conf
-echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf
-echo options root=PARTUUID=${UUID} >> /boot/loader/entries/arch.conf
-
 # Set system password
 passwd
 
@@ -36,6 +20,23 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
+
+drive="$1"
+bootloader_partition="$2"
+UUID=$(blkid -s PARTUUID -o value ${bootloader_partition})
+
+dialog --title "Arch chroot" --yesno "Install bootloader on ${bootloader_partition} with UUID ${UUID}?" 5 50 && echo "ok"
+
+# dialog --title "Dotfile installer" --infobox "Updating pacman mirrors." 3 70
+# reflector --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
+
+dialog --title "Dotfile installer" --infobox "Installing bootloader on ${bootloader_partition} with UUID ${UUID}" 3 50
+bootctl install >/dev/null 2>&1 || echo "Bootctl seemed to hit a snag..."
+echo title Arch Linux >> /boot/loader/entries/arch.conf
+echo linux /vmlinuz-linux >> /boot/loader/entries/arch.conf
+echo initrd /intel-ucode.img >> /boot/loader/entries/arch.conf
+echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf
+echo options root=PARTUUID=${UUID} >> /boot/loader/entries/arch.conf
 
 systemctl enable NetworkManager
 systemctl start NetworkManager
