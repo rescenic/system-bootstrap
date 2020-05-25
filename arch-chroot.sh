1e89e60dc3a19c1243f0fbb93256b569aced0f65
Vlad Doster <mvdoster@gmail.com>
2020-05-25 17:07:59 -0500

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 # Potential variables: timezone, lang, local, drive, and bootloader partition
-pacman --noconfirm -Sy dialog intel-ucode reflector networkmanager 
+pacman --noconfirm --needed -S dialog intel-ucode reflector networkmanager 
 
 if [ $# -ne 2 ]; then
     dialog --title "BOOMER BRAINLET" --msgbox "Something went wrong. Chroot did not receive 2 arguments.\nIt needs the drive and the bootloader partition." 5 50
     exit 1
 fi
+
+# Set system password
+passwd
+
+# Set system timezone
+TZuser=$(cat tzfinal.tmp)
+ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
+hwclock --systohc
 
 systemctl enable NetworkManager
 systemctl start NetworkManager
@@ -15,14 +23,6 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
-
-# Set system timezone
-TZuser=$(cat tzfinal.tmp)
-ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
-hwclock --systohc
-
-# Set system password
-passwd
 
 drive="$1"
 bootloader_partition="$2"
@@ -34,7 +34,7 @@ dialog --title "Arch chroot" --yesno "Install bootloader on ${bootloader_partiti
 # reflector --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
 
 dialog --title "Dotfile installer" --infobox "Installing bootloader on ${bootloader_partition} with UUID ${UUID}" 3 50
-bootctl install >/dev/null 2>&1 || echo "Bootctl seemed to hit a snag..."
+bootctl install || echo "Bootctl seemed to hit a snag..."
 echo title Arch Linux >> /boot/loader/entries/arch.conf
 echo linux /vmlinuz-linux >> /boot/loader/entries/arch.conf
 echo initrd /intel-ucode.img >> /boot/loader/entries/arch.conf
