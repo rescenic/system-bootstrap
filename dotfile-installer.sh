6d655654a868c8bad8bab0b3128dda573de28a87
Vlad Doster <mvdoster@gmail.com>
2020-05-26 01:19:54 -0500

    Update dotfile-installer.sh

--- dotfile-installer.sh
+++ dotfile-installer.sh
@@ -15,7 +15,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 esac done
 
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/vladdoster/dotfiles.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/vladdoster/system-bootstrap/master/dotfile-programs.csv"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/vladdoster/system-bootstrap/master/dotfile-user-programs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
