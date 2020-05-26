3ad6e3e22e3dc10c8d99482ea788f56f2317b2be
Vlad Doster <mvdoster@gmail.com>
2020-05-26 01:58:08 -0500

    Update dotfiles-installer.sh

--- dotfiles-installer.sh
+++ dotfiles-installer.sh
@@ -31,29 +31,31 @@ adduserandpass() { \
 	repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel "$repodir"
 	echo "$name:$pass1" | chpasswd
 	unset pass1 pass2 
-;}
+}
 
 aurinstall() { \
 	dialog --title "Dotfile installer" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 0 0
 	echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
 	sudo -u "$name" $aur_helper -S --noconfirm "$1" >/dev/null 2>&1
-;}
+}
 	
 enabledocker() { \
 	# https://docs.docker.com/install/linux/linux-postinstall/
 	systemctl enable docker.service
 	systemctl start docker.service
 	usermod -aG docker $USER
-;}
+}
 	
 error() { \
-	clear; printf "ERROR:\\n%s\\n" "$1"; exit
-;}
+	clear; 
+	printf "ERROR:\\n%s\\n" "$1";
+	exit
+}
 	
 finalize(){ \
 	dialog --infobox "Preparing welcome message..." 0 0
 	dialog --colors --cr-wrap --title "Dotfiles installed" --msgbox "If no hidden errors, dotfile-installer.sh completed successfully. \nNumber of programs installed -> \\Zb$total\\Zn.\n\n\\ZbPrograms that might not have gotten installed\\Zn:\n$unsuccessfully_installed_programs" 12 80
-;}
+}
 	
 getuserandpass() { \
 	# Prompts user for new username an password.
@@ -68,7 +70,7 @@ getuserandpass() { \
 		pass1=$(dialog --no-cancel --title "Dotfile installer" --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
 		pass2=$(dialog --no-cancel --title "Dotfile installer" --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
 	done 
-;}
+}
 	
 gitmakeinstall() { \
 	progname="$(basename "$1")"
@@ -79,7 +81,7 @@ gitmakeinstall() { \
 	make >/dev/null 2>&1
 	make install >/dev/null 2>&1
 	cd /tmp || return 
-;}
+}
 
 installationloop() { \
 	([ -f "$user_programs_file" ] && cp "${user_programs_file}" /tmp/programs.csv) || curl -Ls "${user-programs-file}" | sed '/^#/d' | eval grep "$grepseq" > "/tmp/${user-programs-file}"
@@ -95,15 +97,15 @@ installationloop() { \
 			*) maininstall "$program" "$comment" ;;
 		esac
 	done < "/tmp/${user_programs_file}"
-;}
+}
 	
 installnvimplugins(){ \
 	nvim +PlugInstall +qall >/dev/null 2>&1 
-;}
+}
 
 installpkg(){ 
 	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 
-;}
+}
 
 maininstall() { \
 	# Installs all needed programs from main repo.
@@ -114,7 +116,7 @@ maininstall() { \
 makedirectories() { \
 	mkdir -p /home/"${name}"/github
 	mkdir -p /home/"${name}"/downloads
-;}
+}
 
 manualinstall() { \
 	# Installs $1 manually if not installed. Used only for AUR helper here.
@@ -127,23 +129,23 @@ manualinstall() { \
 	cd "$1" &&
 	sudo -u "$name" makepkg --noconfirm -si >/dev/null 2>&1
 	cd /tmp || return) 
-;}
+}
 
 newperms() { \
 	# Set special sudoers settings for install (or after).
 	sed -i "/#dotfile-installer/d" /etc/sudoers
 	echo "$* #dotfile-installer" >> /etc/sudoers 
-;}
+}
 
 pipinstall() { \
 	dialog --title "Dotfile installer" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 0 0
 	command -v pip || installpkg python-pip >/dev/null 2>&1
 	yes | pip install "$1"
-;}
+}
 
 preinstallmsg() { \
 	dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 0 0 || { clear; exit; }
-;}
+}
 
 putgitrepo() { \
 	# Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
@@ -154,12 +156,12 @@ putgitrepo() { \
 	chown -R "$name":wheel "$dir" "$2"
 	sudo -u "$name" git clone -b "$branch" --depth 1 "$1" "$dir" >/dev/null 2>&1
 	sudo -u "$name" cp -rfT "$dir" "$2"
-;}
+}
 
 refreshkeys() { \
 	dialog --title "Dotfile installer" --infobox "Refreshing Arch Keyring..." 0 0
 	pacman --noconfirm -Sy archlinux-keyring >/dev/null 2>&1
-;}
+}
 	
 run_reflector(){ \
 	dialog --title "Dotfile installer" --yesno "Install and run reflector? It might speed up package downloads." 0 0
@@ -174,22 +176,22 @@ run_reflector(){ \
      	  return
      	  ;;
 	esac
-;}
+}
 
 systembeepoff() { \
 	dialog --title "Dotfile installer" --infobox "Getting rid of that retarded error beep sound..." 0 0
 	rmmod pcspkr
 	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf 
-;}
+}
 
 usercheck() { \
 	! (id -u "$name" >/dev/null) 2>&1 ||
 	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. Dotfile installer will install for a pre-existing user, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nDotfile installer will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that Dotfile installer will change $name's password to the one you just gave." 0 0
-;}
+}
 	
 welcomemsg() { \
 	dialog --title "Welcome!" --msgbox "Welcome to bootstrapping script!\\n\\nThis script will automatically install a fully-featured Arch Linux desktop." 0 0
-;}
+}
 
 #--- SCRIPT LOGIC ---#
 # Check if user is root on Arch distro. Install dialog.
