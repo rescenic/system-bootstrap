ffbea91e3718aab695d9ec92db24fdf6176dbf8d
Vlad Doster <mvdoster@gmail.com>
2020-05-30 09:55:34 -0500

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -7,10 +7,10 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-# Set system password
+# system password
 passwd
 
-# Set system timezone
+# system timezone
 TZuser=$(cat tzfinal.tmp)
 ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
 hwclock --systohc
@@ -18,7 +18,7 @@ hwclock --systohc
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
-# Set system locale
+# system locale
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
@@ -30,8 +30,8 @@ UUID=$(blkid -s PARTUUID -o value "$drive"3)
 
 dialog --title "Arch chroot" --yesno "Install bootloader on ${bootloader_partition} with UUID ${UUID}?" 0 0 && echo "ok"
 
-# dialog --title "Dotfile installer" --infobox "Updating pacman mirrors." 3 70
-# reflector --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
+dialog --title "Dotfile installer" --infobox "Updating pacman mirrors." 3 70
+reflector --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
 
 dialog --title "Dotfile installer" --infobox "Installing bootloader on ${bootloader_partition} with UUID ${UUID}" 0 0
 bootctl install || echo "Bootctl seemed to hit a snag..."
@@ -42,5 +42,5 @@ echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf
 echo options root=PARTUUID=${UUID} >> /boot/loader/entries/arch.conf
 
 
-installdotfiles() { curl -O https://raw.githubusercontent.com/vladdoster/system-bootstrap/master/dotfile-installer.sh && bash dotfile-installer.sh ;}
-dialog --title "Install dotfiles?" --yesno "Install dotfiles.vdoster.com?" 5 50 && installdotfiles
+install_dotfiles() { curl -O https://raw.githubusercontent.com/vladdoster/system-bootstrap/master/dotfiles-installer.sh && bash dotfiles-installer.sh ;}
+dialog --title "Install dotfiles?" --yesno "Install dotfiles.vdoster.com?" 5 50 && install_dotfiles
