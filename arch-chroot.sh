aa97a9fdfeded644fcc9c4f3d35b8955dc868af4
Vlad Doster <mvdoster@gmail.com>
2020-05-30 10:58:42 -0500

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -4,7 +4,7 @@ pacman --noconfirm --needed -S dialog intel-ucode reflector networkmanager
 
 if [ $# -ne 2 ]; then
     dialog --title "BOOMER BRAINLET" \
-           --msgbox "Something went wrong. Chroot did not receive 2 arguments.\nIt needs the drive and the bootloader partition." \
+           --msgbox "Chroot did not receive 2 arguments.\nIt needs a drive and bootloader partition." \
            0 0
     exit 1
 fi
@@ -26,34 +26,33 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
 
-drive="$1"
-bootloader_partition="$2"
-UUID=$(blkid -s PARTUUID -o value "$drive"3)
-
-dialog --title "Arch chroot" \
-       --yesno "Install bootloader on ${bootloader_partition} with UUID ${UUID}?" \
-       0 0 && echo "ok"
-
-dialog --title "Arch chroot" \
-       --infobox "Updating pacman mirrors." \
-       0 0
-reflector --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
-
-dialog --title "Arch chroot" \
+install_bootloader() { \
+    drive="$1"
+    bootloader_partition="$2"
+    UUID=$(blkid -s PARTUUID -o value "$drive"3)
+    dialog --title "Arch chroot" \
        --infobox "Installing bootloader on ${bootloader_partition} with UUID ${UUID}" \
        0 0
-bootctl install || echo "Bootctl seemed to hit a snag..."
-echo title Arch Linux >> /boot/loader/entries/arch.conf
-echo linux /vmlinuz-linux >> /boot/loader/entries/arch.conf
-echo initrd /intel-ucode.img >> /boot/loader/entries/arch.conf
-echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf
-echo options root=PARTUUID=${UUID} >> /boot/loader/entries/arch.conf
-
+    bootctl install || echo "Bootctl seemed to hit a snag..."
+    echo title Arch Linux >> /boot/loader/entries/arch.conf
+    echo linux /vmlinuz-linux >> /boot/loader/entries/arch.conf
+    echo initrd /intel-ucode.img >> /boot/loader/entries/arch.conf
+    echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf
+    echo options root=PARTUUID=${UUID} >> /boot/loader/entries/arch.conf
+}
 
 install_dotfiles() { \
     curl -O https://raw.githubusercontent.com/vladdoster/system-bootstrap/master/dotfiles-installer.sh 
     bash dotfiles-installer.sh 
 }
+
+run_reflector() { \
+    reflector --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
+}
+
+run_reflector
+
+install_bootloader
 
 dialog --title "Arch chroot" \
        --yesno "Install dotfiles.vdoster.com?" \
