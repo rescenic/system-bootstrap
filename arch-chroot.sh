eb7ae97c959e694fff03c47b20bd957cb09c6240
Vlad Doster <mvdoster@gmail.com>
2020-05-30 10:49:37 -0500

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Potential variables: timezone, lang, local, drive, and bootloader partition
+
 pacman --noconfirm --needed -S dialog intel-ucode reflector networkmanager 
 
 if [ $# -ne 2 ]; then
@@ -34,12 +34,12 @@ dialog --title "Arch chroot" \
        --yesno "Install bootloader on ${bootloader_partition} with UUID ${UUID}?" \
        0 0 && echo "ok"
 
-dialog --title "Dotfile installer" \
+dialog --title "Arch chroot" \
        --infobox "Updating pacman mirrors." \
        0 0
 reflector --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
 
-dialog --title "Dotfile installer" \
+dialog --title "Arch chroot" \
        --infobox "Installing bootloader on ${bootloader_partition} with UUID ${UUID}" \
        0 0
 bootctl install || echo "Bootctl seemed to hit a snag..."
@@ -50,7 +50,11 @@ echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf
 echo options root=PARTUUID=${UUID} >> /boot/loader/entries/arch.conf
 
 
-install_dotfiles() { curl -O https://raw.githubusercontent.com/vladdoster/system-bootstrap/master/dotfiles-installer.sh && bash dotfiles-installer.sh ;}
-dialog --title "Install dotfiles?" \
+install_dotfiles() { \
+    curl -O https://raw.githubusercontent.com/vladdoster/system-bootstrap/master/dotfiles-installer.sh 
+    bash dotfiles-installer.sh 
+}
+
+dialog --title "Arch chroot" \
        --yesno "Install dotfiles.vdoster.com?" \
        0 0 && install_dotfiles
