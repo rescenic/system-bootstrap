0e4c9f86d4727effd055c24dbb4c6c098bc82d9e
Vlad Doster <mvdoster@gmail.com>
2020-05-30 10:47:27 -0500

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -3,7 +3,9 @@
 pacman --noconfirm --needed -S dialog intel-ucode reflector networkmanager 
 
 if [ $# -ne 2 ]; then
-    dialog --title "BOOMER BRAINLET" --msgbox "Something went wrong. Chroot did not receive 2 arguments.\nIt needs the drive and the bootloader partition." 5 50
+    dialog --title "BOOMER BRAINLET" \
+           --msgbox "Something went wrong. Chroot did not receive 2 arguments.\nIt needs the drive and the bootloader partition." \
+           0 0
     exit 1
 fi
 
@@ -28,12 +30,18 @@ drive="$1"
 bootloader_partition="$2"
 UUID=$(blkid -s PARTUUID -o value "$drive"3)
 
-dialog --title "Arch chroot" --yesno "Install bootloader on ${bootloader_partition} with UUID ${UUID}?" 0 0 && echo "ok"
+dialog --title "Arch chroot" \
+       --yesno "Install bootloader on ${bootloader_partition} with UUID ${UUID}?" \
+       0 0 && echo "ok"
 
-dialog --title "Dotfile installer" --infobox "Updating pacman mirrors." 3 70
+dialog --title "Dotfile installer" \
+       --infobox "Updating pacman mirrors." \
+       0 0
 reflector --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
 
-dialog --title "Dotfile installer" --infobox "Installing bootloader on ${bootloader_partition} with UUID ${UUID}" 0 0
+dialog --title "Dotfile installer" \
+       --infobox "Installing bootloader on ${bootloader_partition} with UUID ${UUID}" \
+       0 0
 bootctl install || echo "Bootctl seemed to hit a snag..."
 echo title Arch Linux >> /boot/loader/entries/arch.conf
 echo linux /vmlinuz-linux >> /boot/loader/entries/arch.conf
@@ -43,4 +51,6 @@ echo options root=PARTUUID=${UUID} >> /boot/loader/entries/arch.conf
 
 
 install_dotfiles() { curl -O https://raw.githubusercontent.com/vladdoster/system-bootstrap/master/dotfiles-installer.sh && bash dotfiles-installer.sh ;}
-dialog --title "Install dotfiles?" --yesno "Install dotfiles.vdoster.com?" 5 50 && install_dotfiles
+dialog --title "Install dotfiles?" \
+       --yesno "Install dotfiles.vdoster.com?" \
+       0 0 && install_dotfiles
