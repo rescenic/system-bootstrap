8ff017f7da6ef9044e0eb80c578e31a6ae228f00
Vlad Doster <mvdoster@gmail.com>
2020-05-30 21:21:28 -0500

    Update dotfiles-installer.sh

--- dotfiles-installer.sh
+++ dotfiles-installer.sh
@@ -11,8 +11,9 @@ add_dotfiles() {
     echo "$name"
     git_pkg_clone "$dotfiles_repo" "/home/$name" "$repo_branch"
     rm -f "/home/$name/README.md" "/home/$name/LICENSE"
-    git update-index --assume-unchanged "/home/$name/README.md"
-    git update-index --assume-unchanged "/home/$name/LICENSE"
+    cd /home/$name && \
+    git update-index --assume-unchanged "/home/$name/LICENSE"; && \
+    git update-index --assume-unchanged "/home/$name/README.md";
 }
 
 aur_pkg_install() {
