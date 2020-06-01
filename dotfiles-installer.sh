84b043a34b9bb94c79be275116583e4f5bef9269
Vlad Doster <mvdoster@gmail.com>
2020-05-31 19:58:20 -0500

    Update dotfiles-installer.sh

--- dotfiles-installer.sh
+++ dotfiles-installer.sh
@@ -58,7 +58,7 @@ display_password_input() {
 #   Installer functions   #
 # ======================= #
 add_dotfiles() {
-#     display_info_box "Downloading and installing config files at /home/$name..."
+    display_info_box "Downloading and installing config files at /home/$name..."
     user_home_dir="/home/$name"
     dir=$(mktemp -d)
     [ ! -d "$user_home_dir" ] && mkdir -p "$user_home_dir"
@@ -67,8 +67,7 @@ add_dotfiles() {
     sudo -u "$name" cp -rfT "$dir" "$user_home_dir"
     rm -f "$user_home_dir/README.md" "$user_home_dir/LICENSE"
     # make git ignore deleted LICENSE & README.md files
-#     git update-index --assume-unchanged "$user_home_dir/README.md"
-#     git update-index --assume-unchanged "$user_home_dir/LICENSE"
+    cd "$user_home_dir" && git update-index --assume-unchanged "$user_home_dir/README.md" "$user_home_dir/LICENSE"
 }
 
 arch_pkg_install() {
