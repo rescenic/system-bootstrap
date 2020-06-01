e8d69dcbc7890b7cd04d5b30ca909be48be6195c
Vlad Doster <mvdoster@gmail.com>
2020-05-31 21:59:22 -0500

    Update dotfiles-installer.sh

--- dotfiles-installer.sh
+++ dotfiles-installer.sh
@@ -121,7 +121,7 @@ get_user_credentials() {
 }
 
 git_pkg_install() {
-    progname="$(basename "$1" .git)"
+    progname="$(basename "$1")"
     dir="$repodir/$progname.git"
     display_info_box "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2"
     sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return ; sudo -u "$name" git pull --force origin master;}
